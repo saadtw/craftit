a8459246ef439d0d5be028d614c785297addb769
saadtw <fa22-bse-016@cuilahore.edu.pk>
2026-05-04 13:46:42 +0500

    Remove temp files

--- fix_admin_profile.py
+++
@@ -1,7 +0,0 @@
-import re
-
-with open('app/admin/profile/page.js', 'r', encoding='utf-8') as f:
-    text = f.read()
-
-# We need to manually construct the merged version of app/admin/profile/page.js
-# and app/manufacturer/settings/page.js
